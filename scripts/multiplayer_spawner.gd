dd7f7e656f078e5a9b94db3f31171769a583c1cb
Nathan Lesmann <38919317+EitherRock@users.noreply.github.com>
2026-03-26 19:26:45 -0500

    Create player for server

--- scripts/multiplayer_spawner.gd
+++ scripts/multiplayer_spawner.gd
@@ -5,7 +5,6 @@ extends MultiplayerSpawner
 func _ready() -> void:
 	multiplayer.peer_connected.connect(spawn_player)
 	
-
 func spawn_player(id: int) -> void:
 	if !multiplayer.is_server(): return
 	
@@ -13,3 +12,15 @@ func spawn_player(id: int) -> void:
 	player.name = str(id)
 	
 	get_node(spawn_path).call_deferred('add_child', player) 
+
+func spawn_server_player() ->void:
+	var server_id = multiplayer.get_unique_id()
+	var player: Node = network_player.instantiate()
+	player.name = str(server_id)
+	
+	get_node(spawn_path).call_deferred('add_child', player)
+
+
+func _on_ui_server_started() -> void:
+	if multiplayer.is_server():
+		spawn_server_player()
